6763581414c2a4f944ac2f0d685158f1f0bd2f17
Bouzazi <hammabouzazi@live.fr>
2019-06-26 11:00:03 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Component from './components/Component'
+
+function App() {
+   return (
+      <>
+         <Component/>
+      </>
+   )
+}
+
+export default App;
